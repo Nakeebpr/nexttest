35bcf39eace7cb190304fe59df523e6a5dfec6ab
Nakeebpr <rautnakeeb@gmail.com>
2023-09-11 17:40:01 +0530

    Adding updated config files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
-}
+  experimental: {
+    serverActions: true,
+  },
+  output: "export",
+  distDir: "_static",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
